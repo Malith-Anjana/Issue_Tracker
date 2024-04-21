db5d03c70dfc118d4f81ff34f2161872c6577183
Malith Anjana <malithanjana97@gmail.com>
2024-04-21 21:45:27 +0530

    Update Issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,7 @@
 import { IssueSchema } from '@/app/validationSchemas';
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+
 export async function PATCH (request:NextRequest, {params}: {params: {id:string}}){
     const body = await request.json();
     const validation = IssueSchema.safeParse(body);
@@ -8,20 +9,19 @@ export async function PATCH (request:NextRequest, {params}: {params: {id:string}
     if(!validation.success)
         return NextResponse.json(validation.error.format(), {status:400})
     
-    const issue = prisma.issue.findUnique({
+    const issue = await prisma.issue.findUnique({
         where: {id: parseInt(params.id)}
     })
-
     if(!issue)
-        return NextResponse.json({error:"Invalid Data"}, {status: 400});
+        return NextResponse.json({error:"Invalid Data"}, {status: 404});
 
-    const updatedIssue = prisma.issue.update({
-        where: {id: parseInt(params.id)},
+    const updatedIssue = await prisma.issue.update({
+        where: {id: issue.id},
         data: {
             title: body.title,
             description:body.description
         }
     })
 
-    return NextResponse.json(updatedIssue, {status: 200});
+    return NextResponse.json(updatedIssue);
 }
