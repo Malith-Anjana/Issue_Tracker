28a3c514cbf4eb2f676aa9e786ee807b7664b9f6
Malith Anjana <malithanjana97@gmail.com>
2024-04-25 21:06:53 +0530

    Build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Pagination from './components/Pagination'
+import LatestIssues from './LatestIssues'
 
-export default function Home({searchParams}: {searchParams: { page: string}}) {
+export default function Home() {
   return (
-   <main>
-    <Pagination itemCount={20} currentPage={parseInt(searchParams.page)} pageSize={5}/>
-    </main>
+    <LatestIssues/>
   )
 }
